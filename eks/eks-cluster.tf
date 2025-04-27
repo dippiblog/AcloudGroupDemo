d0dae42411f32d083bb68b420a60436a038cabb1
dippi786 <38568643+dippi786@users.noreply.github.com>
2025-04-27 22:06:24 +0530

    updated

--- eks/eks-cluster.tf
+++ eks/eks-cluster.tf
@@ -1,3 +1,4 @@
+# EKS Module Configuration
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.0.4"
@@ -11,15 +12,12 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
-
       instance_types = ["t3.small"]
-
       min_size     = 1
       max_size     = 3
       desired_size = 2
@@ -27,12 +25,22 @@ module "eks" {
 
     two = {
       name = "node-group-2"
-
       instance_types = ["t3.small"]
-
       min_size     = 1
       max_size     = 2
       desired_size = 1
     }
+  }
+}
+
+# Backend Configuration for Remote State Storage in S3 with DynamoDB for State Locking
+terraform {
+  backend "s3" {
+    bucket         = "948048451133terraform-state-bucket"   # Replace with your actual S3 bucket name
+    key            = "eks/terraform.tfstate"       # Path to store the state file in the bucket
+    region         = "us-west-2"                   # Ensure this matches your AWS region
+    encrypt        = true                          # Enable encryption for state file storage
+    dynamodb_table = "terraform-state-lock"        # Ensure the DynamoDB table for state locking exists
+    acl            = "bucket-owner-full-control"   # Optional but recommended for controlling bucket permissions
   }
 }
