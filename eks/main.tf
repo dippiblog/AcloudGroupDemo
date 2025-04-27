b32eb70656ebef294ae97de85fe510dc974cd4d6
dippi786 <38568643+dippi786@users.noreply.github.com>
2025-04-27 23:27:45 +0530

    updated

--- eks/main.tf
+++ eks/main.tf
@@ -1,10 +1,19 @@
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "aws" {
   region = var.region
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
 }
 
 data "aws_availability_zones" "available" {}
