9ab5a3141754b866282418aa2f911a915f695fca
dippi786 <38568643+dippi786@users.noreply.github.com>
2025-04-27 14:08:16 +0530

    just commented in main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# Terraform EKS Cluster with VPC and IAM Roles
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
