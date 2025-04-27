bbd4dd05d8bb6b8a06f5b47c1afe776f6684243e
dippi786 <38568643+dippi786@users.noreply.github.com>
2025-04-27 18:04:06 +0530

    cluster version upgraded

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "19.10.0"
 
   cluster_name    = local.cluster_name
-  cluster_version = "2.32"
+  cluster_version = "1.32"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
